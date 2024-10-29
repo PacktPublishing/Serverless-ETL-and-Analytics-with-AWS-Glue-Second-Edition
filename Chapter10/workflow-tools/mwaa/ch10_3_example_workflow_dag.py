722af209ea19059c85d0713a65068af3365759a3
Tom Tanaka <tanaka1201t@gmail.com>
2024-10-30 02:05:00 +0900

    Migrated DAG script to the latest version and change the desc of state machine

--- Chapter10/workflow-tools/mwaa/ch10_3_example_workflow_dag.py
+++ Chapter10/workflow-tools/mwaa/ch10_3_example_workflow_dag.py
@@ -1,42 +1,36 @@
-from datetime import timedelta  
-import airflow  
-from airflow import DAG  
-from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
-from airflow.providers.amazon.aws.operators.glue_crawler import AwsGlueCrawlerOperator
-
+from datetime import timedelta
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.providers.amazon.aws.operators.glue_crawler import GlueCrawlerOperator
+from airflow.utils.dates import days_ago
 
 
 default_args = {  
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(1),
     'retries': 0,
     'retry_delay': timedelta(minutes=2),
     'provide_context': True
 }
 
-dag = DAG(  
-    dag_id='ch10_3_example_workflow_mwaa',
+with DAG(
     default_args=default_args,
-    dagrun_timeout=timedelta(hours=2),
-    schedule_interval='0 3 * * *'
-)
-
-glue_crawler_sales = AwsGlueCrawlerOperator(
-    task_id="sales_crawl",
-    config={"Name": "ch10_3_example_workflow"},
-    dag=dag)
-
-glue_job_gen_report = AwsGlueJobOperator(  
-    task_id="gen_report",  
-    job_name='ch10_3_example_workflow_gen_report',  
-    script_args={
-        "--datalake_location": "s3://<your-bucket-and-path>",
-        "--database": "<your-database>",
-        "--table": "example_workflow_mwaa_sales",
-        "--report_year": "2021",
-        "--extra-jars": "s3://crawler-public/json/serde/json-serde.jar"
-        },
-    dag=dag)
-
-glue_crawler_sales >> glue_job_gen_report
+    dag_id='ch10_3_example_workflow_mwaa',
+    schedule_interval='0 3 * * *',
+    start_date=days_ago(1),
+    dagrun_timeout=timedelta(hours=2)
+):
+    crawl_sales_data = GlueCrawlerOperator(
+            task_id='sales_crawl',
+            config={"Name": "ch10_3_example_workflow"})
+    gen_report = GlueJobOperator(  
+                task_id='gen_report',
+                job_name='ch10_3_example_workflow_gen_report',
+                script_args={
+                    '--datalake_location': '<s3://<your-bucket-and-path>',
+                    '--database': '<your-database>',
+                    '--table': 'example_workflow_mwaa_sales',
+                    '--report_year': '2024',
+                    '--extra-jars': 's3://crawler-public/json/serde/json-serde.jar'
+                })
+    crawl_sales_data >> gen_report
